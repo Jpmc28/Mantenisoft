7773d90fe5406545c0e3161520073dad711414b2
Juan Martin <juanpmartin28@gmail.com>
2024-11-24 17:22:15 +0000

    se deja interfaz de inicio graficamente completa, se agrega icono de la clinica en el proyecto

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
-    <title>nombre</title>
+    <link rel="website icon" href="img/GtuzsKu2ryrS5m0Z-removebg-preview1.png">
+    <title>Mantenisoft</title>
 </head>
 <body>
     <div id="seccion_video">
